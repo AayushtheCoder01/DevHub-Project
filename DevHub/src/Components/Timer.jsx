66ff850cbb292d1ccffee77e893a238070ea8fe9
Aayush Kumar <aayushkumarkumar1234@gmail.com>
2024-01-16 13:24:58 +0530

    commit3

--- DevHub/src/Components/Timer.jsx
+++ DevHub/src/Components/Timer.jsx
@@ -110,7 +110,7 @@ export default function Timer() {
         <input onChange={checkAndSetSeconds} value={seconsds} type='number' max={60} min={1}/>
 
             <sub>
-                <span className='text-lg text-white p-1'>secs</span>
+                <span className='text-lg text-white p-1'>sec</span>
             </sub>
         </div>
     </div>
