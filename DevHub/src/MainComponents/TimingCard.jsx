dee48dce0b1cd1462911785e374fc5b975533006
Aayush Kumar <aayushkumarkumar1234@gmail.com>
2024-01-16 13:30:32 +0530

    commit5

--- DevHub/src/MainComponents/TimingCard.jsx
+++ DevHub/src/MainComponents/TimingCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useState, useRef } from 'react'
 import Stopwatch from '../Components/Stopwatch'
-import Timer from '../Components/Timer'
+import Timer from '../Components/Timer.jsx'
 
 function Timing() {
     const [mode, setMode] = useState(true)
